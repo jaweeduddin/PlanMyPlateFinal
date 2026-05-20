e0d86f8a3f5ddb026194a8c3b49846884bd58303
Abrar <k.jaweeduddin@gmail.com>
2026-05-20 14:32:04 +0530

    remove venv

--- api/index.py
+++ api/index.py
@@ -3,7 +3,7 @@ from groq import Groq
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../templates", static_folder="../static")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = "planmyplate_secret"
