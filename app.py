9130d8d3db1a9b4f056289f5ab244176ccd25f66
Abrar <k.jaweeduddin@gmail.com>
2026-05-20 13:29:37 +0530

    Fixed os import

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, session
 from groq import Groq
 from flask_sqlalchemy import SQLAlchemy
+import os
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 
